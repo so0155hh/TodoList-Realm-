d928742351bbc147a848e051be5159fbb2d78197
Nozomi <nozomi.kuwahara@gmail.com>
2020-02-15 22:50:41 +0900

    Realmの練習

---
+++ TodoList/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  TodoList
+//
+//  Created by 桑原望 on 2020/02/15.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
